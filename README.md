278aa7486ce0820925d472249ee72bb0f5c913e0
Regina Bianchini de Miranda da Matta <regina.bianchinidemirandadamatta@student.saultcollege.ca>
2026-04-07 11:32:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CSD214 w26 Lecture 8 Starter App
+- [Lecture 8...](https://docs.google.com/document/d/1DqL55GGoQbX6LO9NgkX5eCzmMfDRyoH_PEh4T4bo9QQ/edit?usp=sharing)
---
+++ src/test/java/csd214/app/Bookstore20251204Lecture08W26SpringBootIocStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package csd214.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bookstore20251204Lecture08W26SpringBootIocStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
